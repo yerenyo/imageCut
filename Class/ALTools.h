3c685780c7a5250accf38db8755df87837b481ce
GuJinyou <yerenyo@gmail.com>
2014-10-17 18:07:13 +0800

    UI文件

---
+++ Class/ALTools.h
@@ -0,0 +1,21 @@
+//
+//  ALTools.h
+//  imageCut
+//
+//  Created by GuJinyou on 14-10-17.
+//  Copyright (c) 2014年 Qoocc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+typedef enum{
+    kImageCutNine=9,
+    kImageCutSix=6,
+    kImageCutFour=4,
+    kImageCutThree=3,
+    kImageCutTwo=2
+}kImageCutType;
+
+
+@interface ALTools : NSObject
+
+@end
